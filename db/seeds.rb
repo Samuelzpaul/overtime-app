1aca2ff3902a0978986c4c2012cac5dca7bf2102
Samuel Paul <spaul1522@gmail.com>
2018-01-17 13:47:53 -0700

    added phone attributes to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "test@test.com", 
+                    password: "asdfasdf", 
+                    password_confirmation: "asdfasdf", 
+                    first_name: "Jon", 
+                    last_name: "Snow",
+                    phone: "2087541007")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+AdminUser.create(email: "admin@test.com", 
+                 password: "asdfasdf", 
+                 password_confirmation: "asdfasdf", 
+                 first_name: "Admin", 
+                 last_name: "Name",
+                 phone: "2087541007")
 
 puts "1 Admin User created"
 
